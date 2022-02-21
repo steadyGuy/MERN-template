8da49ac0d2468a9c7a3f122fab5d931b1a29cb62
Yuriy Soproniuk <sopronukCC@gmail.com>
2022-02-21 22:49:14 +0200

    configured

---
+++ backend/routes/index.ts
@@ -0,0 +1,9 @@
+import express from "express";
+import UserController from "../controllers/UserController";
+
+const router = express.Router();
+
+router.get("/users", UserController.getUsers);
+
+
+export default router;
