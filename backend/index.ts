ed2885ed35cd67651cb2a016edaec8f1a246d893
Yuriy Soproniuk <sopronukCC@gmail.com>
2022-02-22 22:41:18 +0200

    refactroing

--- backend/index.ts
+++ backend/index.ts
@@ -1,11 +1,12 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
 dotenv.config();
-import express from 'express';
-import cors from 'cors';
 
-import router from './routes';
-import './config/db';
+import express from "express";
+import cors from "cors";
+
+import router from "./routes";
+import "./config/db";
 
 const app = express();
 
@@ -13,10 +14,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 // TODO: Change asterisc for something more particular
-app.use(cors({ credentials: true, origin: '*' }));
+app.use(cors({ credentials: true, origin: "*" }));
 
 // routes middleware
-app.use('/api', router);
+app.use("/api", router);
 
 // turn on the server
 const port = process.env.PORT || 3000;
